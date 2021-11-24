55e08459484af2ec94068c5b34582be89d442a45
Pedram Elmi <pedram.elmi@gmail.com>
2021-11-24 05:50:51 +0330

    Fixed pmpy __init__.py possition

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-"""PmPy is a Project Management Tool in Python
-"""
-print("hello form pmpy, project management library  developed at KNTU")
-
-#from pmpy.des import *
---
+++ src/pmpy/__init__.py
@@ -0,0 +1,5 @@
+"""PmPy is a Project Management Tool in Python
+"""
+print("Hello from pmpy, project management package developed at KNTU (https://github.com/Project-PmPy/pmpy)")
+
+__version__ = "1.0.4"

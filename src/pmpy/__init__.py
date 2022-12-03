45480e13d154eb6132d208e543e457a7d6d7c966
Naimeh Sadeghi <53972421+NaimaSadeghi@users.noreply.github.com>
2022-12-03 09:14:31 +0330

    Add files via upload

--- src/pmpy/__init__.py
+++ src/pmpy/__init__.py
@@ -1,5 +1,5 @@
 """PmPy is a Project Management Tool in Python
 """
-print("Hello from pmpy, project management package developed at KNTU (https://github.com/Project-PmPy/pmpy)")
-print("Current subpackage includes des (discrete event simulatoin), dists (distributions), mcs (Monte Carlo simulation) modules, and consim(Contious Simulation).")
+print("Hello from pmpy, project management package (https://github.com/Project-PmPy/pmpy)")
+print("Current subpackage includes des (discrete event simulatoin), consim (continuous simulation), abs(agent-based simulation), mcs(monte carlo simulation), and dists(probability distributions) modules.")
 __version__ = "1.0.14"

6475837998eb80f728fc62cf6fe371ee1bbb4312
Pedram Elmi <pedram.elmi@gmail.com>
2023-07-02 19:25:54 +0330

    added dist members to simpm init

--- src/simpm/__init__.py
+++ src/simpm/__init__.py
@@ -1,6 +1,7 @@
 """SimPM is a Simulation Tool in Project Management (https://github.com/Project-SimPM/SimPM). Current subpackage includes des (discrete event simulation), and dists(probability distributions) modules.
 """
 from simpm.des import *
+from simpm.dist import *
 from simpm.log_cfg import log_config, logger
 
-__version__ = "2.0.1"
+__version__ = "2.0.2"

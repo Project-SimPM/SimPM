2a7618001f5cb1c9f1db518f6f663bf1e29990d1
Pedram Elmi <pedram.elmi@gmail.com>
2024-05-15 03:21:22 +0330

    added readthedocs files

---
+++ docs/source/conf.py
@@ -0,0 +1,76 @@
+# Configuration file for the Sphinx documentation builder.
+# All configuration values have a default; values that are commented out serve to show the default.
+
+import sys
+import os
+from datetime import date
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, "../../example")
+sys.path.insert(0, "../../src/simpm")
+
+# -- General configuration ------------------------------------------------
+
+# General information about the project.
+project = "SimPM"
+copyright = f"2021-{date.today().year}, Project SimPM Team"
+author = 'Project SimPM Team'
+release = '0.1'
+version = '0.1.0'
+
+# If your documentation needs a minimal Sphinx version, state it here.
+# needs_sphinx = '1.0'
+
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'recommonmark',
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# The master toctree document.
+master_doc = "index"
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+# -- Options for HTML output ----------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = 'sphinx_rtd_theme'
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = "images/simpm_logo.png"
+
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+html_favicon = "images/simpm_logo.ico"
+
+# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
+html_show_sphinx = False
+
+# If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
+# html_show_copyright = True
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
+
---
+++ docs/source/index.rst
@@ -0,0 +1,23 @@
+SimPM - Simulation Tool in Project Management
+===================================
+
+SimPM offers a discrete event simulation library for project management in Python.
+It pulls data from the `Open Food Facts database <https://world.openfoodfacts.org/>`_
+and offers a *simple* and *intuitive* API.
+
+.. include:: ../../README.md
+    :parser: markdown
+
+
+.. note::
+
+   This document is under active development.
+   SimPM has its documentation hosted on Read the Docs.
+
+
+Contents
+--------
+
+.. toctree::
+
+   api
